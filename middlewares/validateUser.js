535ed93aea1cd6813df628c5fd7a7dfaa0ed30ba
Jess Daniel <jessdaniel474@gmail.com>
2019-12-04 18:29:47 -0500

    added post, delete, and put methods

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -1,7 +1,7 @@
 const validateUser = (req, res, next) => {
     const data = req.body;
     console.log(data);
-    if (!data) {
+    if (Object.keys(data).length === 0) {
         res.status(400).json({ message: "missing user data" });
     } else if (!data.name) {
         res.status(400).json({ message: "missing required name field" });
--- users/userRouter.js
+++ users/userRouter.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+// Database functions
 const Users = require("./userDb");
+const Posts = require("../posts/postDb");
 
 // middlewares
 const validateUser = require("../middlewares/validateUser");
 const validateUserId = require("../middlewares/validateUserId");
+const validatePost = require("../middlewares/validatePost");
 
 router.post('/', validateUser, (req, res) => {
   // do your magic!
@@ -20,8 +23,18 @@ router.post('/', validateUser, (req, res) => {
   })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const { id } = req.user;
+  const postData = { ...req.body, user_id: id };
+  Posts.insert(postData)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "serer error", err });
+  })
+
 });
 
 router.get('/', (req, res) => {
@@ -44,20 +57,45 @@ router.get('/:id', validateUserId, (req, res) => {
   res.status(200).json(req.user);
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  Posts.get(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "No posts were found for this user" });
+    }
+  })
+    .catch(err => {
+      res.status(500).json({ message: "server error", err });
+  })
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  const { id } = req.user;
+  const user = req.user;
+  Users.remove(id)
+    .then(removed => {
+      res.status(200).json({ message: "User removed successfully", user });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "server error", err });
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res) => {
   // do your magic!
+  const changes = req.body;
+  const { id } = req.user;
+  const updatedUser = { ...changes, id };
+  Users.update(id, changes)
+    .then(user => {
+      res.status(200).json({ message: "User updated successfully", updatedUser });
+    })
+    .catch(err => {
+      res.status(500).json({ message: "server error", err });
+  })
 });
-
-function validatePost(req, res, next) {
-  // do your magic!
-}
 
 module.exports = router;
